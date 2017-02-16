be20c4d98503621f74b455a5962c71c13ce92b27
zenghui420 <miaozh12@126.com>
2017-02-16 10:46:18 +0800

    Add file.

--- baidu-js/models.js
+++ baidu-js/models.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var _ = require('underscore');
 
 module.exports = function(wagner) {
-    mongoose.connect('mongodb://localhost:27017/test');
+    mongoose.connect('mongodb://4:27017/test');
 
     var User =
         mongoose.model('User', require('./user'), 'users');
