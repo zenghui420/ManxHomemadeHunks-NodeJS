aaeee0e048272fc8097961aaba68550e8e952fbd
zenghui420 <miaozh12@126.com>
2017-02-21 21:10:15 +0800

    Change the html structure to fit fetching data from database -3

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ app.use(express.static(__dirname + "/public"));
 var io = require("socket.io").listen(app.listen(port));
 
 io.sockets.on('connection', function (socket) {
-   socket.on('request', function () {
-      io.sockets.emit('response', {name: 'Lotus'});
+   socket.on('request', function (data) {
+      io.sockets.emit('response', {name: data.request});
    });
 
 });
--- public/js/main.js
+++ public/js/main.js
@@ -28,49 +28,47 @@ jQuery(document).ready(function($){
             event.preventDefault();
             var selectedItem = $(this);
             var socket = io.connect('http://localhost:2046');
-            socket.emit('request', {request: "events"});
+
+            var selectedTab = selectedItem.data('content'),
+                selectedSchedule = tabContentWrapper.find('.cd-schedule');
+
+            socket.emit('request', {request: selectedTab});
+
             socket.on('response', function(event) {
                 if( !selectedItem.hasClass('selected') ) {
-                    // var selectedTab = selectedItem.data('content');
-                        // selectedContent = tabContentWrapper.find('div[data-content="'+selectedTab+'"]'),
-                        // selectedSchedule = selectedContent.find('.cd-schedule');
+                    var eventsContent =tabContentWrapper.find('.events-group');
 
-                    // var eventsContent = selectedContent.find('.events-group');
+                    for (var i=0; i<eventsContent.length; i++) {
+                        var topInfo = eventsContent[i].getElementsByClassName('top-info');
+                        var unOrdered = eventsContent[i].getElementsByTagName('ul')[0];
 
-                    // for (var i=0; i<eventsContent.length; i++) {
-                    //     var topInfo = eventsContent[i].getElementsByClassName('top-info');
-                    //     var unOrdered = eventsContent[i].getElementsByTagName('ul')[0];
-                    //
-                    //     $(unOrdered).empty();
-                    //
-                    //     var listItem = document.createElement('li');
-                    //
-                    //     listItem.setAttribute('class','single-event');
-                    //     listItem.setAttribute('data-start','09:30');
-                    //     listItem.setAttribute('data-end','11:00');
-                    //     listItem.setAttribute('data-content','event-abs-circuit');
-                    //     listItem.setAttribute('data-event','event-1');
-                    //
-                    //     var anchorEvent = document.createElement('a');
-                    //
-                    //     anchorEvent.setAttribute('href','#0');
-                    //
-                    //     var emphasized = document.createElement('em');
-                    //
-                    //     emphasized.setAttribute('class','event-name');
-                    //
-                    //     var eventName = document.createTextNode(event.name);
-                    //
-                    //     emphasized.appendChild(eventName);
-                    //     anchorEvent.appendChild(emphasized);
-                    //     listItem.appendChild(anchorEvent);
-                    //     unOrdered.appendChild(listItem);
-                    // }
+                        $(unOrdered).empty();
 
-                    // new SchedulePlan($(selectedSchedule)).placeEvents();
+                        var listItem = document.createElement('li');
 
-                    // var selectedsubContent = selectedContent.find('div'),
-                    //     selectedContentHeight = selectedContent.find('body').innerHeight();
+                        listItem.setAttribute('class','single-event');
+                        listItem.setAttribute('data-start','09:30');
+                        listItem.setAttribute('data-end','11:00');
+                        listItem.setAttribute('data-content','event-abs-circuit');
+                        listItem.setAttribute('data-event','event-1');
+
+                        var anchorEvent = document.createElement('a');
+
+                        anchorEvent.setAttribute('href','#0');
+
+                        var emphasized = document.createElement('em');
+
+                        emphasized.setAttribute('class','event-name');
+
+                        var eventName = document.createTextNode(event.name);
+
+                        emphasized.appendChild(eventName);
+                        anchorEvent.appendChild(emphasized);
+                        listItem.appendChild(anchorEvent);
+                        unOrdered.appendChild(listItem);
+                    }
+
+                    new SchedulePlan($(selectedSchedule)).placeEvents();
 
                     var selectedContentHeight = tabContentWrapper.find('body').innerHeight();
 
