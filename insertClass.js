091b3194310b9fd579d0b88a6ce2090c44c9e22b
zenghui420 <miaozh12@126.com>
2017-02-23 20:28:09 +0800

    DataBase

--- insertClass.js
+++ insertClass.js
@@ -2,14 +2,14 @@
  * Created by Edmund on 2/22/2017.
  */
 
-var ClassA = require('../models/ClassA');
+var ClassA = require('models/ClassA');
 var mongoose = require('mongoose');
-var dbConfig = require('./db');
+var dbConfig = require('.db');
 var fs = require('fs');
 
 mongoose.connect(dbConfig.url);
 
-fs.readFile('ClassAData.json', 'utf8', function (err, data) {
+fs.readFile('./data/ClassAData.json', 'utf8', function (err, data) {
     if (err) throw err; // we'll not consider error handling for now
     var obj = JSON.parse(data);
     // console.log(obj[0].teacher);
