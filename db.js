3da104ebf42c13cdbd443c0029a2d09a4646b05b
zenghui420 <miaozh12@126.com>
2017-02-24 11:29:45 +0800

    DataBase finally done

--- db.js
+++ db.js
@@ -1,3 +1,7 @@
+// module.exports = {
+//     'url': 'mongodb://45.76.100.30/test/?connectTimeoutMS=300000&socketTimeoutMS=300000'
+// }
+
 module.exports = {
-    'url': 'mongodb://45.76.100.30/test/?connectTimeoutMS=300000&socketTimeoutMS=300000'
+    'url': 'mongodb://45.76.100.30/test'
 }
